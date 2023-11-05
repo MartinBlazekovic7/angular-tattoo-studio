9645bfee6a2307ab4a03a0c8412f63780b7269ca
Martin Blažeković <martinblazekovic7@gmail.com>
2023-11-05 23:54:24 +0100

    initial file and package set up

---
+++ src/app/pages/designs/designs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DesignsRoutingModule } from './designs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DesignsRoutingModule
+  ]
+})
+export class DesignsModule { }
