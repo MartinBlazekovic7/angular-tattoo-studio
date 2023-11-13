1c18e836813fea6f64989ca74c7fe6fa062355ab
Martin Blažeković <martinblazekovic7@gmail.com>
2023-11-14 00:32:41 +0100

    create home page

--- src/app/pages/home/components/home/home.component.ts
+++ src/app/pages/home/components/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '@service/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  showingLogin = true;
 
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {}
 }
