59929992b54b3afff71d50fa78771df460a57a86
Martin Blažeković <martinblazekovic7@gmail.com>
2023-11-14 01:22:25 +0100

    design changes

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.scss']
+  styleUrls: ['./navigation-bar.component.scss'],
 })
 export class NavigationBarComponent {
-
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let navbar = document.querySelector('.navigation-bar') as HTMLElement;
+    let header = document.querySelector('.header-image') as HTMLElement;
+    if (window.scrollY > header.clientHeight - 100) {
+      navbar.classList.add('navbar-inverse');
+    } else {
+      navbar.classList.remove('navbar-inverse');
+    }
+  }
 }
