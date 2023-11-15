eac923af426a87a4a4b9ae3021688ed616f7ba2a
Martin Blažeković <martinblazekovic7@gmail.com>
2023-11-16 00:15:55 +0100

    create forbidden and not found pages

--- src/app/pages/forbidden/forbidden-routing.module.ts
+++ src/app/pages/forbidden/forbidden-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ForbiddenComponent } from './components/forbidden/forbidden.component';
 
-const routes: Routes = [{ path: '', component: ForbiddenComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ForbiddenComponent,
+    data: {
+      title: 'Forbidden.',
+      description: '',
+      image:
+        "background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('assets/images/headers/forbidden-not-found-header.jpg')",
+      isHomePage: false,
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/not-found/not-found-routing.module.ts
+++ src/app/pages/not-found/not-found-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [{ path: '', component: NotFoundComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: NotFoundComponent,
+    data: {
+      title: 'Not found.',
+      description: '',
+      image:
+        "background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('assets/images/headers/forbidden-not-found-header.jpg')",
+      isHomePage: false,
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
