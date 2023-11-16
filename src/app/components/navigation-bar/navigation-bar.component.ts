b60401fde4f1d82a87664cd8b1fc053b9e76daa0
Martin Blažeković <martinblazekovic7@gmail.com>
2023-11-16 15:08:14 +0100

    add responsive design

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -6,13 +6,23 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss'],
 })
 export class NavigationBarComponent {
+  showMenu: boolean = false;
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
     let navbar = document.querySelector('.navigation-bar') as HTMLElement;
+    let navbarMobile = document.querySelector('.navbarMobile') as HTMLElement;
     if (window.scrollY > navbar.clientHeight) {
       navbar.classList.add('navbar-inverse');
     } else {
       navbar.classList.remove('navbar-inverse');
+    }
+    if (window.scrollY > navbarMobile.clientHeight) {
+      navbarMobile.classList.add('navbar-inverse');
+    } else {
+      navbarMobile.classList.remove('navbar-inverse');
     }
   }
 }
