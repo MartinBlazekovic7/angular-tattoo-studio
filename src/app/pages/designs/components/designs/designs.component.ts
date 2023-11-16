7762198b8b108a97ef891a7c398833f70f7bd673
Martin Blažeković <martinblazekovic7@gmail.com>
2023-11-16 04:32:49 +0100

    add sorting and filtering to designs page

--- src/app/pages/designs/components/designs/designs.component.ts
+++ src/app/pages/designs/components/designs/designs.component.ts
@@ -19,6 +19,7 @@ export class DesignsComponent implements OnInit {
   tags: Tag[] = [];
 
   designs: Design[] = [];
+  designsFiltered: Design[] = [];
   queryArgs = qs.stringify({
     populate: [
       'slug',
@@ -31,17 +32,85 @@ export class DesignsComponent implements OnInit {
     ],
   });
 
+  typeFilterLabel?: string = 'Type';
+  selectedType?: Type | null;
+
+  sortLabel?: string = 'Popularity (DESC)';
+
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
-    this.api.getResources('designs', this.queryArgs).subscribe((data) => {
-      this.designs = data.data as Design[];
-    });
     this.api.getResources('types').subscribe((data) => {
       this.types = data.data as Type[];
     });
     this.api.getResources('tags').subscribe((data) => {
       this.tags = data.data as Tag[];
     });
+    this.api.getResources('designs', this.queryArgs).subscribe((data) => {
+      this.designs = data.data as Design[];
+      this.applyFilters();
+    });
+  }
+
+  applyFilters(): void {
+    this.filterByType(null);
+  }
+
+  filterByType(type: Type | null): void {
+    console.log(type);
+    if (type) {
+      this.selectedType = type;
+      console.log(this.designs);
+      this.designsFiltered = this.designs.filter(
+        (item) => item.attributes?.type?.data?.id === type.id
+      );
+    } else {
+      this.selectedType = null;
+      this.designsFiltered = this.designs;
+    }
+    console.log(this.designsFiltered);
+    this.typeFilterLabel = this.selectedType?.attributes?.name || 'Type';
+  }
+  sortDesigns(sortType: string): void {
+    switch (sortType) {
+      case 'price-asc':
+        this.sortLabel = 'Price (ASC)';
+        this.designsFiltered.sort(
+          (a, b) => a?.attributes?.price! - b?.attributes?.price!
+        );
+        break;
+      case 'price-desc':
+        this.sortLabel = 'Price (DESC)';
+        this.designsFiltered.sort(
+          (a, b) => b?.attributes?.price! - a?.attributes?.price!
+        );
+        break;
+      case 'alphabetical-asc':
+        this.sortLabel = 'Alphabetical (ASC)';
+        this.designsFiltered.sort((a, b) =>
+          a?.attributes!.name!.localeCompare(b?.attributes!.name!)
+        );
+        break;
+      case 'alphabetical-desc':
+        this.sortLabel = 'Alphabetical (DESC)';
+        this.designsFiltered.sort((a, b) =>
+          b.attributes!.name!.localeCompare(a?.attributes!.name!)
+        );
+        break;
+      case 'popularity-asc':
+        this.sortLabel = 'Popularity (ASC)';
+        this.designsFiltered.sort(
+          (a, b) => a?.attributes?.popularity! - b?.attributes?.popularity!
+        );
+        break;
+      case 'popularity-desc':
+        this.sortLabel = 'Popularity (DESC)';
+        this.designsFiltered.sort(
+          (a, b) => b?.attributes?.popularity! - a?.attributes?.popularity!
+        );
+        break;
+      default:
+        console.error('Invalid sortType:', sortType);
+    }
   }
 }
